d5bd4923b18589109214e3b959660f4d7140fe1b
tdrose <tim@rose-4-you.de>
2024-04-04 09:52:22 +0000

    Revision of kb stats figure

--- config.py
+++ config.py
@@ -24,6 +24,12 @@ module_key = 'module_analysis'
 module_dir = os.path.join(store_dir, module_key)
 if module_key not in os.listdir(store_dir):
     os.mkdir(module_dir)
+    
+# Subfolder for saving module analysis related files
+figuredata_key = 'figure_data'
+figuredata_dir = os.path.join(store_dir, figuredata_key)
+if figuredata_key not in os.listdir(store_dir):
+    os.mkdir(figuredata_dir)
 
 # For R scripts to have access to the data directories
 analysis_config_dict = {'date_key': date_key,
@@ -31,6 +37,8 @@ analysis_config_dict = {'date_key': date_key,
                         'store_dir': store_dir, 
                         'enrichment_key': enrichment_key, 
                         'enrichment_dir': enrichment_dir, 
+                        'figuredata_key': figuredata_key,
+                        'figuredata_dir': figuredata_dir,
                         'module_key': module_key,
                         'module_dir': module_dir}
 
